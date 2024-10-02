db382461f40e3cdc03341a1ccb2d0cc3915723fb
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-10-02 00:51:43 -0300

    feat: create users and restaurants tables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,13 @@
 import type { Config } from "drizzle-kit"
+import { env } from "./src/env"
 
 const config: Config = {
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
   dialect: 'postgresql',
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  }
 } 
 
 export default config
